26f4f391dc0cff9794a61e18c2d70ede9eac6d5f
Arindam Das <arindamdas@Arindams-MacBook-Air.local>
2020-03-21 20:33:58 +0530

    ready for deployment

--- src/core/labels.py
+++ src/core/labels.py
@@ -1,36 +1,53 @@
 import pandas as pd
 import io
-import labels
-from reportlab.graphics import shapes
-from django.http import FileResponse
 
-def draw_label(label, width, height, obj):
-    
-    label.add(shapes.String(5, 60, 'BIG FOX', fontName="Courier", fontSize=8))
-    label.add(shapes.String(50, 60, obj.Product_Name, fontName="Courier", fontSize=6))
-    label.add(shapes.String(5, 50, 'SKU    :', fontName="Courier", fontSize=8))
-    label.add(shapes.String(50, 50, str(obj.SKU), fontName="Courier", fontSize=8))
-    label.add(shapes.String(5, 40, 'COLOR  :', fontName="Courier", fontSize=8))
-    label.add(shapes.String(50, 40, obj.Color, fontName="Courier", fontSize=8))
-    label.add(shapes.String(5, 30, 'SIZE   :', fontName="Courier", fontSize=8))
-    label.add(shapes.String(50, 30, str(obj.Size), fontName="Courier", fontSize=8))
-    label.add(shapes.String(5, 20, 'MRP    :', fontName="Courier", fontSize=8))
-    label.add(shapes.String(50, 20, str(obj.MRP), fontName="Courier", fontSize=8))
-    label.add(shapes.String(5, 10, 'MARKED BY - SIDDHARTH SALES', fontName="Courier", fontSize=8))
-    label.add(shapes.String(5, 3, 'E-MAIL: siddharthsales5@gmail.com', fontName="Courier", fontSize=6))
+from reportlab.graphics.barcode import qr
+from reportlab.graphics.shapes import Drawing 
+from reportlab.lib.units import mm
+from reportlab.pdfgen import canvas
+from reportlab.graphics import renderPDF
+
 
 def get_labels(data):
     buffer = io.BytesIO()
-    specs = labels.Specification(210, 297, 3, 8, 50, 25, corner_radius=1)
+    data = data.fillna({'QTY': 0})
+    c = canvas.Canvas(buffer, bottomup=0, pagesize=(75*mm,50*mm))
 
-    sheet = labels.Sheet(specs, draw_label, border=True)
-
-    for idx, product in data.iterrows():
-        for i in range(0, product.QTY):
-            sheet.add_label(product)
-    
-    sheet.save(buffer)
+    for index, row in data.iterrows():
+        mrp = '{:0,.2f}'.format(row.MRP)
+        for i in range(0, int(row.QTY)):
+            textobject = c.beginText()
+            textobject.setTextOrigin(3*mm, 5*mm)
+            textobject.setFont('Courier', 10)
+            textobject.textLine(text='Big Fox - ' + row.Product_Name)
+            c.drawText(textobject)
+            textobject.setFont('Courier', 14)
+            textobject.textLine(text=' ')
+            c.drawText(textobject)
+            textobject.textLine(text='SKU      : ' + row.SKU)
+            c.drawText(textobject)
+            textobject.textLine(text='Color    : ' + row.Color)
+            c.drawText(textobject)
+            textobject.textLine(text='Size     : ' + str(row.Size))
+            c.drawText(textobject)
+            textobject.textLine(text='Price    : ' + mrp)
+            c.drawText(textobject)
+            textobject.textLine(text=' ')
+            c.drawText(textobject)
+            textobject.setFont('Courier', 8)
+            textobject.textLine(text='MARKED BY - SIDDHARTH SALES')
+            c.drawText(textobject)
+            textobject.textLine(text='E-MAIL: siddharthsales5@gmail.com')
+            c.drawText(textobject)
+            # qr_code
+            qr_code = qr.QrCodeWidget(f'Big_FOX<{row.SKU}_{row.Product_Name}_{row.Color}_{row.Size}_{mrp}>')
+            bounds = qr_code.getBounds()
+            width = bounds[2] - bounds[0]
+            height = bounds[3] - bounds[1]
+            d = Drawing(20*mm, 20*mm, transform=[20./width,0,0,20./height,0,0])
+            d.add(qr_code)
+            renderPDF.draw(d, c,65*mm, 40*mm)
+            c.showPage()
+    c.save()
     buffer.seek(0)
     return buffer
-
-    
